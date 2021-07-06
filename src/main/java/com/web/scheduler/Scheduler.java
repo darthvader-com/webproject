f4c3b725c3b0efc5b4b10a5a45d466bd5ab5ed83
AHJ <superfantastic@kakao.com>
2021-07-06 23:12:18 +0900

    배치 시간 수정

--- src/main/java/com/web/scheduler/Scheduler.java
+++ src/main/java/com/web/scheduler/Scheduler.java
@@ -18,11 +18,11 @@ public class Scheduler {
 	SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 	String dateStr1 = sdf1.format(new Date());
 
-	// 매일 오전 10시 10분 간격
-	@Scheduled(cron = "0 0/10 10 * * *")
+	// 매일 오전 10시부터 5분 간격
+	@Scheduled(cron = "0 0/5 10 * * *")
 	public void coronaBatch() {
-		System.out.println("10시[10분간격] 배치 호출 시점 ㅣ " + dateStr1);
-		c.insertCoronaBatch("batch - 10시(0/10)배치");
+		System.out.println("10시[5분간격] 배치 호출 시점 ㅣ " + dateStr1);
+		c.insertCoronaBatch("coronaBatch - 10시(5분간격)");
 	}
 
 }
